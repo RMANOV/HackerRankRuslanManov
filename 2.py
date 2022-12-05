7254db5750df6494a4d212c1a71b328abf197789
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-05 16:52:30 +0200

    17

---
+++ 2.py
@@ -0,0 +1,42 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    # Write your code here
+    for i in range(3):
+        if a[i] > b[i]:
+            a[i] = 1
+            b[i] = 0
+        elif a[i] < b[i]:
+            a[i] = 0
+            b[i] = 1
+        else:
+            a[i] = 0
+            b[i] = 0
+    return [sum(a), sum(b)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ Untitled-5.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    ar_sum = 0
+    for i in ar:
+        ar_sum += i
+    return ar_sum
+    # for i in range(0,len(ar)):
+    #     sum=sum+ar[i]
+    # return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
