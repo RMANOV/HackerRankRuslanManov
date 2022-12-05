74882073e0a3c779b563525c1d291b2f64b8f3e6
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-05 17:00:42 +0200

    17.01

--- A Very Big Sum.py
+++ A Very Big Sum.py
@@ -1,2 +1,34 @@
 
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    big_sum = 0
+    for i in ar:
+        big_sum += i
+    return big_sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
