c4f8fc0f017a9baac12b630145ee54755f699414
RMANOV <96174405+RMANOV@users.noreply.github.com>
2022-12-06 10:43:07 +0200

    10.42

---
+++ candles.py
@@ -0,0 +1,41 @@
+
+
+# You are in charge of the cake for a child's birthday. 
+# You have decided the cake will have one candle for each year of their total age. 
+# They will only be able to blow out the tallest of the candles. 
+# Count how many candles are tallest.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    
+    for i in range(len(candles)):
+        if candles[i] == max(candles):
+            return candles.count(candles[i])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
