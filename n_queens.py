83205cefb0f68bdb0d6d0dc8e91d3c291ca0d7f0
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-04-12 18:01:33 +0300

    18.00

--- n_queens.py
+++ n_queens.py
@@ -14,52 +14,45 @@
 
 
 
-
-def is_safe(board, row, col, N):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solve_n_queens_util(board, col, N):
-    if col >= N:
-        return True
-
-    for i in range(N):
-        if is_safe(board, i, col, N):
-            board[i][col] = 1
-
-            if solve_n_queens_util(board, col + 1, N):
-                return True
-
-            board[i][col] = 0
-
-    return False
-
-
 def solve_n_queens(N):
     board = [[0 for _ in range(N)] for _ in range(N)]
-
-    if not solve_n_queens_util(board, 0, N):
+    if not place_queen(board, 0, N):
         print("Not possible")
         return
-
     for row in board:
         print(*row)
 
+def place_queen(board, row, N):
+    if row == N:
+        return True
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            if place_queen(board, row + 1, N):
+                return True
+            board[row][col] = 0
+    return False
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    i = row - 1
+    j = col - 1
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i = row - 1
+    j = col + 1
+    while i >= 0 and j < N:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+    return True
 
 if __name__ == "__main__":
     N = int(input().strip())
     solve_n_queens(N)
-
-
